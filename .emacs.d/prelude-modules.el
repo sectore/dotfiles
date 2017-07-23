b93af8bd562aa3e228ceae96595947991035f6c7
Jens Krause <email@jkrause.io>
2017-07-23 14:28:18 +0200

    Update/simplify emacs config for using w/ `Ubuntu`

--- .emacs.d/prelude-modules.el
+++ .emacs.d/prelude-modules.el
@@ -8,7 +8,7 @@
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
-;; (require 'prelude-evil)
+(require 'prelude-evil)
 
 ;;; Programming languages support
 (require 'prelude-c)
@@ -18,7 +18,7 @@
 (require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
-(require 'prelude-elixir)
+;; (require 'prelude-elixir)
 ;; (require 'prelude-go)
 (require 'prelude-haskell)
 (require 'prelude-js)
@@ -26,13 +26,13 @@
 (require 'prelude-lisp)
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
-(require 'prelude-perl)
+;; (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
-(require 'prelude-scss)
+;; (require 'prelude-scss)
 (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 (require 'prelude-yaml)
