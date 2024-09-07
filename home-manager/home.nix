f9c621dc77e0f45232a0abe559a55f81410bd4ae
jk <47693+sectore@users.noreply.github.com>
2024-09-07 21:43:34 +0200

    xdg.mime.enable = false

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,6 +8,7 @@
     ./modules/starship.nix
     ./modules/rust.nix
     ./modules/unfree.nix
+    ./modules/xdg-mime-enable.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
