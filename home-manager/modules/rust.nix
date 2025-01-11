8435ce20a03d5f5e46169588476afcb6c5ed22c0
jk <47693+sectore@users.noreply.github.com>
2025-01-11 13:41:59 +0100

    remove rust pkgs
    -        loco-cli
    -        pkgs.sea-orm-cli
    -        pkgs.cargo-leptos
    -        pkgs.leptosfmt
    -        dioxus-cli

--- home-manager/modules/rust.nix
+++ home-manager/modules/rust.nix
@@ -1,48 +1,6 @@
 { inputs, pkgs, ... }:
 
 let
-  # latest loco-cli@0.2.8
-  loco-cli =  pkgs.rustPlatform.buildRustPackage rec {
-    pname = "loco-cli";
-    version = "0.2.8";
-    src = pkgs.fetchCrate {
-      inherit pname version;
-      sha256 = "sha256-J02Pt+kKu8BsngHoYHpkf2RY4EHTJdO9F7uA0zvFwU0=";
-    };
-    cargoHash = "sha256-UagkiQ9/3xzUNXR10YY/KIlrata83FiJhIxt6WDJnQQ=";
-    # Ignore tests
-    # `test cli_tests ... FAILED`
-    doCheck = false;
-  };
-
-  # Next dioxus-cli to fix hot-reload (desktop)
-    dioxus-cli = pkgs.rustPlatform.buildRustPackage rec {
-      pname = "dioxus-cli";
-      name = "${pname}-git-${src.rev}";
-      src = pkgs.fetchFromGitHub {
-        owner = "DioxusLabs";
-        repo = "dioxus";
-        rev = "jk/proper-asset-crossplatform";
-        sha256 = "sha256-WyMyb5m0uWkxf6MgHBr8mFDq3X5jwBh82F3ATgikI2c=";
-      };
-      cargoLock = {
-        lockFile = "${src}/Cargo.lock";
-      };
-
-      buildAndTestSubdir = "packages/cli";
-
-      buildInputs = with pkgs; [
-        openssl
-        pkg-config
-      ];
-
-      nativeBuildInputs = with pkgs; [
-        pkg-config
-      ];
-      # don't run tests
-      doCheck = false;
-    };
-
     rustPkgs = import pkgs.path {
         inherit (pkgs) system;
         overlays = [ (import inputs.rust-overlay) ];
@@ -61,11 +19,7 @@ in
         ];
         targets = ["x86_64-unknown-linux-gnu" "wasm32-unknown-unknown"];
       })
-        loco-cli
-        pkgs.sea-orm-cli
-        pkgs.cargo-leptos
-        pkgs.leptosfmt
-        dioxus-cli
+        pkgs.cargo-generate
       ];
       sessionPath = [ "$HOME/.cargo/bin" ];
     };
