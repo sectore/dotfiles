bfec35fa7dfa801408669710d34a30382960db00
jk <47693+sectore@users.noreply.github.com>
2025-01-26 17:35:51 +0100

    restructure

--- home-manager/modules/rust.nix
+++ home-manager/modules/rust.nix
@@ -8,19 +8,17 @@
     overlays = [(import inputs.rust-overlay)];
   };
 in {
-  home = {
-    packages = [
-      (rustPkgs.rust-bin.stable.latest.default.override {
-        extensions = [
-          "rust-src"
-          "rust-analyzer"
-          "rustfmt"
-          "clippy"
-        ];
-        targets = ["x86_64-unknown-linux-gnu" "wasm32-unknown-unknown"];
-      })
-      pkgs.cargo-generate
-    ];
-    sessionPath = ["$HOME/.cargo/bin"];
-  };
+  home.packages = with pkgs; [
+    (rustPkgs.rust-bin.stable.latest.default.override {
+      extensions = [
+        "rust-src"
+        "rust-analyzer"
+        "rustfmt"
+        "clippy"
+      ];
+      targets = ["x86_64-unknown-linux-gnu" "wasm32-unknown-unknown"];
+    })
+    cargo-generate
+  ];
+  home.sessionPath = ["$HOME/.cargo/bin"];
 }
