8fa41927a16b3ba79fd23e5e14857e5ea913a665
jk <47693+sectore@users.noreply.github.com>
2025-08-30 16:25:03 +0200

    typo: `nixgl` -> `nixGL`

--- home-manager/modules/nixgl.nix
+++ home-manager/modules/nixgl.nix
@@ -2,7 +2,7 @@
   # `nixGL` config
   #  https://github.com/nix-community/home-manager/blob/master/docs/manual/usage/gpu-non-nixos.md
   nixGL.packages = nixgl.packages;
-  nixgl.defaultWrapper = "mesa";
-  nixgl.offloadWrapper = "nvidia";
+  nixGL.defaultWrapper = "mesa";
+  nixGL.offloadWrapper = "nvidia";
   nixGL.installScripts = ["mesa" "nvidia"];
 }
