d458fbc5b9b9f00b80effa11cf8c20b973f6bde3
jk <47693+sectore@users.noreply.github.com>
2025-11-11 20:18:01 +0100

    fix(nixgl) new struct `targets.genericLinux.nixGL`

--- home-manager/modules/nixgl.nix
+++ home-manager/modules/nixgl.nix
@@ -1,8 +1,10 @@
 {nixgl, ...}: {
   # `nixGL` config
   #  https://github.com/nix-community/home-manager/blob/master/docs/manual/usage/gpu-non-nixos.md
-  nixGL.packages = nixgl.packages;
-  nixGL.defaultWrapper = "mesa";
-  nixGL.offloadWrapper = "nvidia";
-  nixGL.installScripts = ["mesa" "nvidia"];
+  targets.genericLinux.nixGL = {
+    packages = nixgl.packages;
+    defaultWrapper = "mesa";
+    offloadWrapper = "nvidia";
+    installScripts = ["mesa" "nvidia"];
+  };
 }
